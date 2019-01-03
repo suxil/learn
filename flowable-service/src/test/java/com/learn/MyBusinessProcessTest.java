35ea53f0fd2b0432854a368e469a10ab8c49ba6d
suxi <lu_it_work@163.com>
2019-01-03 21:13:13 +0800

    fix flowable

--- flowable-service/src/test/java/com/learn/MyBusinessProcessTest.java
+++ flowable-service/src/test/java/com/learn/MyBusinessProcessTest.java
@@ -1,5 +1,11 @@
 package com.learn;
 
+import org.flowable.bpmn.converter.BpmnXMLConverter;
+import org.flowable.bpmn.model.BpmnModel;
+import org.flowable.bpmn.model.FlowElement;
+import org.flowable.bpmn.model.Process;
+import org.flowable.bpmn.model.UserTask;
+import org.flowable.common.engine.api.io.InputStreamProvider;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
 import org.flowable.engine.test.Deployment;
@@ -7,6 +13,10 @@ import org.flowable.engine.test.FlowableRule;
 import org.flowable.task.api.Task;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.List;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -17,7 +27,7 @@ import static junit.framework.TestCase.assertEquals;
  */
 public class MyBusinessProcessTest {
 
-    @Rule
+//    @Rule
     public FlowableRule flowableRule = new FlowableRule();
 
     @Test
@@ -32,6 +42,28 @@ public class MyBusinessProcessTest {
 
         taskService.complete(task.getId());
         assertEquals(0, runtimeService.createProcessInstanceQuery().count());
+    }
+
+    @Test
+    public void testConvertBpmnXml() {
+        final InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("SeamanPrepareZyhzTestProcess.bpmn20.xml");
+
+        BpmnXMLConverter bpmnXMLConverter = new BpmnXMLConverter();
+
+        BpmnModel bpmnMode = bpmnXMLConverter.convertToBpmnModel(() -> inputStream, false, false);
+        Process process = bpmnMode.getProcesses().get(0);
+        Collection<FlowElement> flowElements  = process.getFlowElements();
+        for (FlowElement flowElement : flowElements) {
+            if (flowElement instanceof UserTask) {
+                UserTask u = (UserTask) flowElement;
+                List outgoingFlows = u.getOutgoingFlows();
+                System.err.println("outgoingFlows:" + outgoingFlows);
+            }
+            System.err.println(flowElement.getId() + "--->>>>" + flowElement.getName());
+        }
+
+        System.out.println("test");
+
     }
 
 }
